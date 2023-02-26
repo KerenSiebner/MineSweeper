31f110ada6e33d7ea2567f3fd827f94f4669ba39
Keren Siebner <kerens969@gmail.com>
2023-02-26 15:33:03 +0200

    CSS fixes: layout

---
+++ js/game.js
@@ -0,0 +1,244 @@
+'use strict'
+
+// surpresses rightclick menu
+window.oncontextmenu = (e) => {
+    e.preventDefault();
+}
+
+const MINE = '\u{1F525}'
+const LIVE = '\u{1F499}'
+// gBoard – A Matrix containing cell objects: Each cell: 
+var gBoard = []
+var gInterval
+var gStartTime
+var gLives = 3
+var gBestResult
+var gLonelyNeighbor
+
+
+//This is an object by which the board size is set (in this case: 4x4 board and how many mines to put)
+var gLevel = {
+    SIZE: 4,
+    MINE: 2
+}
+
+var gGame = {
+    isON: false,
+    shownCount: 0,
+    secsPassed: 0
+}
+
+function initGame() {
+    if (gInterval) {
+        clearInterval(gInterval)
+        gInterval = null
+    }
+    storeBestTime()
+    renderGameInfo()
+    resetTime()
+    gGame.isON = true
+    gBoard = buildBoard(gLevel.SIZE, gLevel.MINE)
+    renderBoard(gBoard, '.board-container')
+}
+
+
+function cellClicked(elCell, i, j) {
+    var strHTML
+    const isMarked = isBoardMarked()
+    const cell = gBoard[i][j]
+
+    //in case game is over, or already shown or marked - don't allow more clicking
+    if (!gGame.isON || cell.isMarked) return
+
+    //first click
+    if (!gGame.shownCount & !isMarked) {
+        cell.isShown = true
+        onFirstClick(i, j)
+    }
+
+    gGame.shownCount++
+    // renderShownCell(i, j)
+
+    //stepped on mine:
+    if (cell.isMine) {
+        elCell.classList.add('mine')
+        cell.isShown = true
+        gLives--
+        if (!checkGameOver()) livesDecreaseMsg()
+        //condition where game is over
+        if (!gLives) {
+            restartButtonChange(false)
+            gameOverMsg(false)
+            storeBestTime()
+            revealMines()
+            const currTime = gGame.secsPassed
+            gGame.secsPassed = currTime
+            clearInterval(gInterval)
+            gGame.isON = false
+        }
+    }
+    //stepped on cell without a mine
+    else {
+        if (cell.minesAroundCount) {
+            strHTML += `${cell.minesAroundCount}`
+            elCell.innerHTML = strHTML
+            cell.isShown = true
+        } else {
+            cell.isShown = true
+            expandShown(gBoard, i, j)
+        }
+    }
+
+    //check if game over
+    if (checkGameOver()) {
+        const currTime = +gGame.secsPassed
+        console.log('currTime', currTime)
+        gGame.secsPassed = currTime
+        console.log('gGame.secsPassed', gGame.secsPassed)
+        renderGameInfo()
+        clearInterval(gInterval)
+    }
+
+    renderBoard(gBoard, '.board-container')
+    renderGameInfo()
+}
+
+//Called on right click to mark a cell (suspected to be a mine)
+// Search the web (and implement) how to hide the context menu on right click
+
+function cellMarked(elCell, i, j) {
+    const cell = gBoard[i][j]
+
+    if (!gGame.isON || cell.isShown) return
+
+    //if this is the first move of the game start timer
+    if (!isBoardMarked() && !gGame.shownCount) {
+        cell.isMarked = true
+        elCell.classList.add('flag')
+        onFirstClick(i, j)
+    }
+    else if (cell.isMarked) {
+        cell.isMarked = false
+        elCell.classList.remove('flag')
+    } else {
+        cell.isMarked = true
+        elCell.classList.add('flag')
+        if (checkGameOver()) {
+            const currTime = gGame.secsPassed
+            gGame.secsPassed = currTime
+            clearInterval(gInterval)
+            gameOverMsg(true)
+        }
+    }
+    // renderBoard(gBoard, '.board-container')
+}
+
+function onFirstClick(row, col) {
+    startTimer()
+    gGame.isON = true
+    setMineOnBoard({ row: row, col: col })
+    setMinesNegsCount(gBoard)
+    renderBoard(gBoard, '.board-container')
+}
+
+//Game ends when all mines are marked, and all the other cells are shown
+function checkGameOver() {
+    if (gGame.shownCount === (gLevel.SIZE ** 2) - gLevel.MINE + (3 - gLives)) {
+        for (var i = 0; i < gLevel.SIZE; i++) {
+            for (var j = 0; j < gLevel.SIZE; j++) {
+                var currCell = gBoard[i][j]
+                if (currCell.isMine && !currCell.isMarked && !currCell.isShown && gLives) return false
+            }
+        }
+        gameOverMsg(true)
+        storeBestTime()
+        restartButtonChange(true)
+        return true
+    }
+    else return false
+}
+
+
+function gameOverMsg(isWin) {
+    const endGameMsg = document.querySelector('h3')
+    if (isWin) endGameMsg.innerText = 'You Won!'
+    else endGameMsg.innerText = 'You Lost!'
+    endGameMsg.hidden = false
+}
+
+function gameOverCloseMsg() {
+    const endGameMsg = document.querySelector('h3')
+    endGameMsg.hidden = true
+}
+
+//When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
+
+function expandShown(board, cellI, cellJ) {
+    gLonelyNeighbor = []
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            var currCell = board[i][j]
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= board[i].length) continue
+
+            if (currCell.isMine || currCell.isMarked || currCell.isShown) continue
+            else {
+                currCell.isShown = true
+                gGame.shownCount++
+                if (!currCell.minesAroundCount) {
+                    expandShown(gBoard,i,j)
+                }
+            }
+        }
+    }
+}
+
+function changeLevel(level) {
+    if (level === 'beginner') {
+        gLevel.SIZE = 4
+        gLevel.MINE = 2
+    }
+    else if (level === 'medium') {
+        gLevel.SIZE = 8
+        gLevel.MINE = 14
+    }
+    else {
+        gLevel.SIZE = 12
+        gLevel.MINE = 32
+    }
+    resetSmiley()
+    gameOverCloseMsg()
+    restartGame()
+}
+
+function livesDecreaseMsg() {
+    const livesMsg = document.querySelector('.lives-msg')
+    var plural = ' lives'
+    if (gLives === 1) plural = ' life'
+    livesMsg.innerText = `You have ${gLives} ${plural} left! `
+    livesMsg.hidden = false
+    setTimeout(() => { livesMsg.hidden = true }, 1500)
+}
+
+//check if there is a best time in the local storage
+function storeBestTime() {
+    var prevBest = +localStorage.getItem('bestResult');
+    console.log('prevBest', prevBest)
+    const timeToSolve = +gGame.secsPassed
+    console.log('timeToSolve', timeToSolve)
+    //before first game no pb
+    if (!prevBest && !timeToSolve) {
+        gBestResult = null
+        return
+    }
+    //before first game but stored pb 
+    else if (prevBest && !timeToSolve) gBestResult = prevBest
+    //after first game, no stored pb or curr time better then pb
+    else if (!prevBest && timeToSolve) gBestResult = timeToSolve
+    else if (timeToSolve < prevBest) gBestResult = timeToSolve
+    //else time is not better > do nothing
+    else return
+    localStorage.setItem('bestResult', gBestResult)
+}
+
---
+++ js/utils.js
@@ -0,0 +1,220 @@
+'use strict'
+
+
+// Builds the board Set mines at random locations Call setMinesNegsCount() Return the created board
+function buildBoard(size, minesCount) {
+    const board = []
+    for (var i = 0; i < size; i++) {
+        board[i] = []
+        for (var j = 0; j < size; j++) {
+            board[i][j] = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
+        }
+    }
+    return board
+}
+
+// Render the board as a <table> to the page
+
+function renderBoard(board, selector) {
+
+    var strHTML = '<table border="0"><tbody>'
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < board[0].length; j++) {
+
+            const cell = board[i][j]
+            var className = `cell cell-${i}-${j}`
+
+            if (cell.isMine && cell.isShown) className += ' mine'
+            if (cell.isShown) className += ' shown'
+            if (cell.isMarked) className += ' flag'
+
+            strHTML += `<td class="${className}" onclick ="cellClicked(this, ${i},${j})" oncontextmenu ="cellMarked(this, ${i},${j})">`
+
+            if (!cell.isMine && cell.isShown && (cell.minesAroundCount !== 0)) strHTML += `${cell.minesAroundCount}`
+        }
+        strHTML += '</td></tr>'
+    }
+    strHTML += '</tbody></table>'
+
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+
+}
+
+function renderShownCell(row, column) {
+    const elCell = document.querySelector(`.cell-${row}-${column}`)
+    elCell.style.backgroundColor = 'blue'
+}
+
+function countMineNeighbors(cellI, cellJ, board) {
+    var neighborsCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= board[i].length) continue
+
+            if (board[i][j].isMine) neighborsCount++
+        }
+    }
+    return neighborsCount
+}
+
+//Count mines around each cell and set the cell's minesAroundCount.
+function setMinesNegsCount(board) {
+    const size = gLevel.SIZE
+    var currMineCount = 0
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            currMineCount = countMineNeighbors(i, j, gBoard)
+            board[i][j].minesAroundCount = currMineCount
+        }
+        currMineCount = 0
+    }
+}
+
+
+function setMineOnBoard() {
+    const mineCount = gLevel.MINE
+    for (var i = 0; i < mineCount; i++) {
+        var mineIdx = randonMineIdx()
+        var currCell = gBoard[mineIdx.i][mineIdx.j]
+        while (currCell.isMine || currCell.isShown) {
+            mineIdx = randonMineIdx()
+            currCell = gBoard[mineIdx.i][mineIdx.j]
+        }
+        gBoard[mineIdx.i][mineIdx.j].isMine = true
+    }
+}
+
+function randonMineIdx() {
+    var randIdx = {}
+    var row = getRandomIntInclusive(0, gLevel.SIZE - 1)
+    var column = getRandomIntInclusive(0, gLevel.SIZE - 1)
+    randIdx = { i: row, j: column }
+    return randIdx
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function onMouseButton(event) {
+    console.log('MouseEvent.button', MouseEvent.button)
+
+    switch (MouseEvent.button) {
+        case 0:
+            cellClicked(elCell, i, j)
+            break
+        case 2:
+            cellMarked(elCell)
+            break
+    }
+}
+
+function startTimer() {
+    gStartTime = Date.now()
+    gInterval = setInterval(() => {
+        const seconds = (Date.now() - gStartTime) / 1000
+        gGame.secsPassed = seconds.toFixed(0)
+        var elTime = document.querySelector('.table-data span')
+        elTime.innerText = gGame.secsPassed
+        // elTime.innerText = seconds.toFixed(0)
+
+    }, 1);
+}
+
+function resetTime() {
+    var elTime = document.querySelector('.table-data span')
+    elTime.innerText = '0'
+}
+
+function restartGame() {
+    console.log('hi')
+    gBoard = []
+    gStartTime = null
+    gGame.shownCount = 0
+    gLives = 3
+    gGame.secsPassed = 0
+    gameOverCloseMsg()
+    clearInterval(gInterval)
+    resetTime()
+    resetSmiley()
+    initGame()
+}
+
+function restartButtonChange(isWin) {
+    const elRestartButton = document.querySelector('.re-start')
+    if (isWin) elRestartButton.style.backgroundImage = 'url(img/king.png)'
+    else elRestartButton.style.backgroundImage = 'url(img/dead.png)'
+}
+
+function resetSmiley() {
+    const elRestartButton = document.querySelector('.re-start')
+    elRestartButton.style.backgroundImage = 'url(img/smily.png)'
+}
+
+function renderGameInfo() {
+    
+    var level = ''
+    switch (gLevel.SIZE) {
+        case 4: level = 'BEGINNER'
+        break
+        case 8: level = 'MEDIUM'
+        break
+        case 12: level = 'EXPERT'
+        break
+    }
+    var hearts = ''
+    switch (gLives) {
+        case 3: hearts = LIVE + LIVE + LIVE
+            break
+        case 2: hearts = LIVE + LIVE
+        break
+        case 1: hearts = LIVE
+        break
+    }
+
+    var bestTime = gBestResult? gBestResult:'NO PB'
+    
+    var strHTML = '<table class=".data" border="1"><tbody>'
+    strHTML += `<tr><th>&nbspTIME&nbsp</th><th>&nbspBEST TIME&nbsp</th><th>&nbspCOUNT&nbsp</th> <th>&nbsp&nbspLEVEL&nbsp&nbsp</th><th>&nbspLIVES&nbsp</th></tr>`
+    strHTML += `<tr><td><span .time>0</span</td><td>${bestTime}</td><td>${gGame.shownCount}</td><td>&nbsp&nbsp${level}&nbsp&nbsp</td><td>${hearts}</td></tr>`
+    strHTML += '</tbody></table>'
+    
+    
+    const elContainer = document.querySelector(".table-data")
+    elContainer.innerHTML = strHTML
+
+    var elTime = document.querySelector('.table-data span')
+    elTime.innerText = gGame.secsPassed
+}
+
+function isBoardMarked() {
+    const size = gBoard.length
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            if (gBoard[i][j].isMarked) return true
+        }
+    }
+    return false
+}
+
+function revealMines() {
+    const size = gBoard.length
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            const currCell = gBoard[i][j]
+            if (currCell.isMine) currCell.isShown = true
+        }
+    }
+    return false
+}
+
